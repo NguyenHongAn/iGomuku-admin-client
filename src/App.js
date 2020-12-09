497ef64972bd0ca911541f76c4efbc6a3ec2c6bc
Nguyen Hong An <37919921+NguyenHongAn@users.noreply.github.com>
2020-12-09 20:32:35 +0700

    [React] - SignIn signUp for admin account

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {HashRouter, Switch, Route} from 'react-router-dom';
 
+import LoginPage from './containers/Signin';
+import SignupPage from './containers/Signup';
+import AppBar from './components/AppBar/AppBar';
+import { ToastProvider } from "react-toast-notifications";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
+      <ToastProvider>
+      <AppBar></AppBar>
       <Switch>
+      
+      <Route path='/auth/signin'>
+            <LoginPage></LoginPage>
+          </Route>
+          <Route path='/auth/signup'>
+            <SignupPage></SignupPage>
+          </Route>
         <Route path='/'>
-          <h1>Home page</h1>
+          
         </Route>
       </Switch>
-    </BrowserRouter>
+      </ToastProvider>
+    </HashRouter>
   );
 }
 
