22ae09c4f753ff5b8001b0178c38363e74e99070
The Anh <nhoxtheanh@gmail.com>
2021-01-07 09:27:42 +0700

    [UI] Try to apply Coreui template

--- src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import socketReducer from './socketReducer';
-
-//combine các reduer lại với nhau 
-const allReducers = combineReducers({
-   socketReducer,
-})
-
-export default allReducers;
